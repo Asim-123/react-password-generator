7240978c20def4fd242cfce26f5f73efd06a6afc
unknown <Asim-123>
2023-06-24 03:54:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import Header from './components/header/Header';
 function App() {
+  const [text, setText] = useState("");
+  const [number, setNumber] = useState("");
+  const [generatedPassword, setGeneratedPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const password = generatePassword();
+    console.log(password);
+    setGeneratedPassword(password);
+  };
+
+  const generatePassword = () => {
+    let password = "";
+    const symbols = "!#@$"
+    const characters = text + number + symbols;
+    const length = Math.min(parseInt(number), 20); 
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters.charAt(randomIndex);
+    }
+
+    return password;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header/>
+    <div className="container">
+      <div className="text">
+        Password Generator
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-row">
+          <div className="input-data">
+            <input type="text" required onChange={(e) => setText(e.target.value)} value={text} />
+            <div className="underline"></div>
+            <label htmlFor="">Type Text</label>
+          </div>
+          <div className="input-data">
+            <input type="number" required onChange={(e) => setNumber(e.target.value)} value={number} />
+            <div className="underline"></div>
+            <label htmlFor="text">Type number</label>
+          </div>
+          <div className="input-data">
+            <input type="" value={generatedPassword} />
+            <div className="underline"></div>
+            <label htmlFor="">Generated password</label>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="input-data textarea">
+            <div className="form-row submit-btn">
+              <div className="input-data">
+                <div className="inner"></div>
+                <input type="submit" value="Generate" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
+    </>
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div className='header'>Built by Asim</div>
+  )
+}
+
+export default Header
